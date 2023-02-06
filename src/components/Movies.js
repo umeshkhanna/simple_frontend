d49b897a238c678414e031e4cea17471d4dc51ad
UmeshkhannaM <umeshkhannan@gmail.com>
2023-02-06 23:55:08 +0530

    cors and backend api connected

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,25 +5,22 @@ const Movies = () => {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        let movieListings = [
-            {
-                id: 1,
-                title: "harry potter",
-                description: "magic movie",
-                release_date: "2000-11-22",
-                runtime: 116,
-                mpaa_rating: "PG-13",
-            },
-            {
-                id: 2,
-                title: "Raiders of the lost ark",
-                description: "adventure",
-                release_date: "1981-06-12",
-                runtime: 115,
-                mpaa_rating: "PG-13",
-            }
-        ];
-        setMovies(movieListings)
+        const headers = new Headers()
+        headers.append("content-type", "application/json")
+
+        const requestOptions = {
+            method: "GET",
+            headers: headers
+        }
+
+        fetch(`http://localhost:8080/movies`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }, [])
     return (
         <div>
